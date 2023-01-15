a98de57bdf400eabe38d8be4639d16d0d0b730a4
Abhishek Marrivagu <abhi.codes10@gmail.com>
2023-01-16 03:42:21 +0530

    fmt clean

--- code/src/chp1.rs
+++ code/src/chp1.rs
@@ -3,13 +3,6 @@
 // f : A -> B , g : B -> C
 // create g(f(a)) : A -> C
 
-fn f<A, B>(a: A) -> B {
-    todo!()
-}
-fn g<B, C>(b: B) -> C {
-    todo!()
-}
-
 fn c<A, B, C>(f: fn(A) -> B, g: fn(B) -> C) -> impl Fn(A) -> C {
     move |a| g(f(a))
 }
@@ -23,5 +16,5 @@ fn mul_2(i: i32) -> i32 {
 
 pub fn call() {
     let composition = c(add_1, mul_2);
-    println!("{}", composition(3));
+    assert_eq!(8, composition(3));
 }
